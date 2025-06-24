0fb52b364480d5943eec8907c8c66389cd6a9c2a
myczh <bore_124@outlook.com>
2025-06-24 22:35:39 +0800

    feat:路由修改至布局

--- lazy-ctrl-ui/src/router/index.tsx
+++ lazy-ctrl-ui/src/router/index.tsx
@@ -1,21 +1,53 @@
+// src/router/index.tsx
 import {
   createRootRoute,
   createRoute,
   createRouter,
   RouterProvider,
   Outlet,
+  lazyRouteComponent,
 } from '@tanstack/react-router'
-import { lazyRouteComponent } from '@tanstack/react-router'
+import MainLayout from '@/layouts/MainLayout'
 
-const Root = () => <Outlet />
+// 根路由（只包一层 Outlet）
+const rootRoute = createRootRoute({
+  component: () => <Outlet />,
+})
 
-const rootRoute = createRootRoute({ component: Root })
+// Layout 路由，负责渲染 tab 栏
+const layoutRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  component: () => <MainLayout />,
+  id: 'layout',
+})
+
+// 子页面：Home
+const homeRoute = createRoute({
+  path: '/',
+  getParentRoute: () => layoutRoute,
+  component: lazyRouteComponent(() => import('@/pages/Home')),
+})
+
+// // 子页面：Config
+// const configRoute = createRoute({
+//   path: '/config',
+//   getParentRoute: () => layoutRoute,
+//   component: lazyRouteComponent(() => import('@/pages/Config')),
+// })
+
+// // 子页面：About
+// const aboutRoute = createRoute({
+//   path: '/about',
+//   getParentRoute: () => layoutRoute,
+//   component: lazyRouteComponent(() => import('@/pages/About')),
+// })
 
 const routeTree = rootRoute.addChildren([
-  createRoute({ 
-    path: '/', 
-    getParentRoute: () => rootRoute,
-    component: lazyRouteComponent(() => import('@/pages/Home')) }),
+  layoutRoute.addChildren([
+    homeRoute,
+    // configRoute,
+    // aboutRoute,
+  ]),
 ])
 
 export const router = createRouter({ routeTree })
